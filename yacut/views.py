10ff73ffa3d1bd9f9b6052ced81c5af156c6d059
Матвей Слуцких <poustplay@yandex.ru>
2023-10-15 19:57:19 +0300

    view

--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,44 @@
 from .import app
 from flask import render_template
+import random
+import string
+from forms import LinkForm
+from models import URLMap
+from . import app
+from flask import redirect, render_template, url_for
+
+
+LENGTH_OF_LINK = 6
+
+
+def get_unique_short_id():
+    return ''.join(
+        random.choices(
+            string.ascii_letters + string.digits,
+            k=LENGTH_OF_LINK
+        )
+    )
 
 
 @app.route('/')
 def index_view():
-    return render_template('index.html')
+    form = LinkForm()
+    if form.validate_on_submit():
+        original_link = form.original_link.data
+        new_link = get_unique_short_id()
+        while URLMap.query.filter_by(short=new_link).first():
+            new_link = get_unique_short_id()
+        url_map = URLMap(
+            original=original_link,
+            short=new_link
+        )
+        db.session.add(url_map)
+        db.session.commit()
+        return redirect(url_for('link_view', id=url_map.id))
+    return render_template('index.html', form=form)
+
+
+@app.route('/<int:id>')
+def link_view(id):
+    link = URLMap.query.get_or_404(id)
+    return render_template('index.html', link=link)
